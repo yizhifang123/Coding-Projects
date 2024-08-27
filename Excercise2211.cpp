0575d6bb58951b1f6221094b719dfe980ec0db02
yizhifang123 <70450152+yizhifang123@users.noreply.github.com>
2024-08-26 21:31:21 -0700

    small issue 2212, put comparison outside main func

--- Excercise2211.cpp
+++ Excercise2211.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
-#include<tuple>
+#include <tuple>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
-  vector<tuple<int, string, string>> people;
-  tuple<int, string, string> person;
-  while(cin >> person){
-     cin >> person;
-     people.push_back(person);
-  }
-  bool custom_comparison(const tuple<int, string, string> a, int, string, string> b){
-      if (get<0>(a) != get<0>(b)) {
-          return get<0>(a) > get<0>(b);
-      }
-      if (get<1>(a) != get<1>(b)) {
-          return get<1>(a) < get<1>(b);
-      }
-      return get<2>(a) < get<2>(b);
-  }
-  sort(people.begin(), people.end(), custom_comparison);
+bool custom_comparison(const tuple<int, string, string>& a, const tuple<int, string, string>& b) {
+    if (get<0>(a) != get<0>(b)) {
+        return get<0>(a) > get<0>(b);
+    }
+    if (get<1>(a) != get<1>(b)) {
+        return get<1>(a) < get<1>(b);
+    }
+    return get<2>(a) < get<2>(b);
+}
+
+int main() {
+    vector<tuple<int, string, string>> people;
+    int id;
+    string first_name, last_name;
+
+    while (cin >> id >> first_name >> last_name) {
+        people.emplace_back(id, first_name, last_name);
+    }
+
+    sort(people.begin(), people.end(), custom_comparison);
+
+    for (const auto& person : people) {
+        cout << get<0>(person) << ' ' << get<1>(person) << ' ' << get<2>(person) << endl;
+    }
+
+    return 0;
 }
--- Excercise2212.cpp
+++ Excercise2212.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
-int main(){
-  vector<pair<int ,int>> fractions;
-  int numerator, denominator;
-  while (cin >> numerator >> denominator) {
-      fractions.push_back(make_pair(numerator, denominator));
-  }
+bool customComparison(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.first * b.second > b.first * a.second;
+}
 
-  bool customComparison(const pair<int, int> a, const pair<int, int> b){
-    float a_frac = a.first/a.second;
-    float b_frac = b.first/b.second;
-    return a_frac > b_frac;
-  }
-  sort(fractions.begin(), fractions.end(), customComparison);
+int main() {
+    vector<pair<int, int>> fractions;
+    int numerator, denominator;
+    while (cin >> numerator >> denominator) {
+        fractions.push_back(make_pair(numerator, denominator));
+    }
+
+    sort(fractions.begin(), fractions.end(), customComparison);
+
+    for (const auto& frac : fractions) {
+        cout << frac.first << '/' << frac.second << endl;
+    }
+
+    return 0;
 }
