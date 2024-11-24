6b75d87594074c2fd33517b0e6533152fb565ac4
yizhifang123 <70450152+yizhifang123@users.noreply.github.com>
2024-11-23 16:49:41 -0800

    finished censoring.cpp

---
+++ USACO Bronze/censoring.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    freopen("censor.in", "r", stdin);
+    freopen("censor.out", "w", stdout);
+    string s;
+    cin >> s;
+    string sub;
+    cin >> sub;
+    int size = s.size();
+    int size2 = sub.size();
+    string ans;
+    for (int i = 0; i < size; i++)
+    {
+        ans += s[i];
+        if (ans.size() > size2)
+        {
+            if(ans.substr(ans.size() - size2) == sub){
+                ans.resize(ans.size() - size2);
+            }
+        }
+    }
+    cout << ans;
+}
